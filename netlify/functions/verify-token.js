c7be589896b77760dc38f5d3c0e0567c51738e6f
i11umeretreat-spec <i11umeretreat@gmail.com>
2026-06-06 16:33:10 +0800

    Update verify-token.js

--- netlify/functions/verify-token.js
+++ netlify/functions/verify-token.js
@@ -39,7 +39,7 @@ const TRACKS = {
     'track-10': { file: 'release/crock.mp3', title: 'Крокодил: обнуление тревоги', type: 'Метафорический сеанс гипноза' },
     'track-11': { file: 'release/immune_booster.mp3', title: 'Иммунный бустер', type: 'Аудиопрактика' },
     // Добавить когда Катя загрузит:
-    // 'track-12': { file: 'release/weight_loss.mp3', title: 'Сброс лишнего веса', type: 'Глубинная перестройка' },
+    'track-12': { file: 'release/weight_release.mp3', title: 'Сеанс самогипноза, снижение веса', type: 'Глубинная перестройка' },
     // 'track-13': { file: 'release/three_totems.mp3', title: 'Три Тотема', type: 'Ресурсный транс' },
     // 'track-14': { file: 'release/goals.mp3', title: 'Достижение целей', type: 'Активация целевого мышления' },
 };
